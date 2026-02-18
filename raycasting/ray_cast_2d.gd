0487ffd88880f58c45bb960f28e28b6e74842d39
Layne Chant <layne.chant@mymail.champlain.edu>
2026-02-18 18:38:18 -0500

    More work on raycasting

--- raycasting/ray_cast_2d.gd
+++ raycasting/ray_cast_2d.gd
@@ -1,18 +1,24 @@
 extends RayCast2D
-
-
 @export var ore: Area2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	
-	target_position = to_local(ore.position)
+	# get the starting pos of the raycast
+	# get the target position 
+	# measure that distance to decide 
+	
+	var starting_location = position
+
+	var ore_shape = ore.get_node("CollisionShape2D")
+	var half_size = ore_shape.shape.size / 2
+	
+	var dir_to_player = (global_position - ore.global_position).normalized()
+	
+	
+	target_position = to_local(ore.global_position + dir_to_player * half_size)
+
 	
 	if is_colliding():
-	
-		print("Colliding!!", position)
+		if get_collider() == ore:
+			print("Colliding!!", position)
